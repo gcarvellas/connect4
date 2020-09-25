79fd6312108ba8e0db03a8b649e8364d0cab3570
gcarvellas <gcarvel264@gmail.com>
2020-09-25 09:08:51 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,15 @@
+public class Player {
+
+	// Constructor
+	public Player() {
+	}
+
+	// Accessor
+	public boolean isComputer() {
+		return false;
+	}
+	
+	public int getMove() {
+		return 0;
+	}
+}
